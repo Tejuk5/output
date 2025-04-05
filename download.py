6ec51440aa0e7d73e6dd73331aebcc2f348c8b99
Tejuk5 <tejukumar1979@gmail.com>
2025-04-05 11:05:55 +0530

    Add files via upload

---
+++ download.py
@@ -0,0 +1,13 @@
+import urllib
+import urllib.request
+import os
+
+
+
+
+file_path = os.path.join("datasets","housing")
+os.makedirs(file_path,exist_ok=True)
+file_path = os.path.join(file_path,"housing.csv")
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv", file_path)
+
